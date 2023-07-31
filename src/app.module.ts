664a9391a9a15415dc281959ad7150180fd23eb6
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-07-31 23:45:29 +0300

    feat: add file structure for artists, tracks & albums

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DbModule } from './db/db.module';
 import { DbService } from './db/db.service';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
-  imports: [UserModule, DbModule],
+  imports: [UserModule, DbModule, TrackModule, AlbumModule, ArtistModule],
   controllers: [AppController],
   providers: [AppService, DbService],
 })

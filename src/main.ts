8ccb7273dfa787d4e6fba30bafa55f3d8a9a4951
Katsiaryna Andrabaila <androbailoe@gmail.com>
2023-08-01 20:01:40 +0300

    feat: add open api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
+    .setVersion('1.0.0')
+    .addTag('home-library', 'music-library')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(app.get(ConfigService).get('PORT', 4000));
 }
 bootstrap();
